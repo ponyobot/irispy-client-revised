4a74df737649820079000fe7df9a3699da6c2511
ponyobot <ponyobot0000@gmail.com>
2026-03-29 11:45:28 +0900

    Allow the bot to respond to its own messages

--- iris/bot/__init__.py
+++ iris/bot/__init__.py
@@ -35,14 +35,14 @@ class Bot:
         self.iris_ws_endpoint = f"ws://{self.iris_url}/ws"
         self.api = IrisAPI(f"http://{self.iris_url}")
 
-    def __process_chat(self, chat: ChatContext):
+    def __process_chat(self, chat: ChatContext): 
         self.emitter.emit("chat", [chat])
         if chat.is_lite:
             self.emitter.emit("message", [chat])
             return
 
         origin = chat.message.v.get("origin")
-        if origin == "MSG":
+        if origin == "MSG" or origin == "WRITE": # 추가됨 1
             self.emitter.emit("message", [chat])
         elif origin == "NEWMEM":
             self.emitter.emit("new_member", [chat])
